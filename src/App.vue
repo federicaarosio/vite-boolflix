db9433a37a1bea7e2bdf85c0dd285f174a1da52d
federicaarosio <arosio.fede@gmail.com>
2023-12-20 12:38:18 +0100

    added empty header and main components

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template lang="">
+  <div>
+    <AppHeader />
+    <AppMain />
+  </div>
+</template>
+<script>
+import { store } from './js/store.js';
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
+
+export default {
+  components: {
+    AppHeader,
+    AppMain
+  },
+
+  data() {
+    return {
+      store
+    }
+  },
+}
+</script>
+<style lang="scss">
+@use './styles/general.scss' as *;
+  
+</style>
